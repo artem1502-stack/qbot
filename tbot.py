c174d0cde17e1ba3d93f200ebe7414e978a81b7d
artem1502-stack <artemwork1502@gmail.com>
2021-04-09 14:50:14 +0300

    1.1

--- tbot.py
+++ tbot.py
@@ -1,14 +1,16 @@
 NAME="qioqiijeqifqieikcsmkkovqwef_bot"
 TOKEN="1664123952:AAEM-8BZAhGcKWu5kTGvB-gvzovde5yIFrE"
 import telebot as tb
-bot = tb.TeleBot("TOKEN")
-@bot.message_handler(commands=[])
+bot = tb.TeleBot(TOKEN)
+@bot.message_handler(commands=['start','help'])
 def send_welcome(message):
 	bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
 	if message.text.lower() == 'привет':
 		bot.send_message(message.from_user.id, 'Привет!')
+	elif message.text.lower() == 'мур':
+		bot.send_message(message.from_user.id, "Мяу")
 	else:
 		bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
 bot.polling(none_stop=True)
